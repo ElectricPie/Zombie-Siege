b5001ba4927533f35574c41e9795026f797a9c22
Connor Priest <Kon_Priest@Hotmail.com>
2024-06-28 15:07:52 +0100

    Units now attack barricades before targeting the player

--- Source/ZombieSiege/Private/Barricade.cpp
+++ Source/ZombieSiege/Private/Barricade.cpp
@@ -12,8 +12,13 @@ ABarricade::ABarricade()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = BaseComponent;
+	
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	RootComponent = Mesh;
+	Mesh->SetupAttachment(RootComponent);
+	Mesh->SetCanEverAffectNavigation(false);
+	Mesh->SetCollisionProfileName(TEXT("NoCollision"));
 	
 	OuterTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Outside Trigger"));
 	OuterTrigger->SetupAttachment(RootComponent);
@@ -25,14 +30,33 @@ ABarricade::ABarricade()
 	InsideDirection->SetupAttachment(RootComponent);
 }
 
-FVector ABarricade::GetOutsideTriggerPos()
+FVector ABarricade::GetOutsideTriggerPos() const
 {
 	return OuterTrigger->GetComponentLocation();
+}
+
+float ABarricade::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
+	AActor* DamageCauser)
+{
+	float const DamageDealt = FMath::Clamp(DamageAmount, 0.f, CurrentHealth); 
+	
+	if (CurrentHealth > 0)
+	{
+		CurrentHealth -= DamageDealt;
+	}
+
+	if (CurrentHealth <= 0)
+	{
+		Mesh->SetVisibility(false);
+	}
+
+	return DamageDealt;
 }
 
 // Called when the game starts or when spawned
 void ABarricade::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	CurrentHealth = MaxHealth;
 }
--- Source/ZombieSiege/Private/Barricade.h
+++ Source/ZombieSiege/Private/Barricade.h
@@ -18,9 +18,16 @@ public:
 	ABarricade();
 
 	UFUNCTION(BlueprintCallable, BlueprintPure)
-	FVector GetOutsideTriggerPos();
+	FVector GetOutsideTriggerPos() const;
 
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsDestroyed() const { return CurrentHealth <= 0; };
+	
 protected:
+	UPROPERTY()
+	USceneComponent* BaseComponent;
 	UPROPERTY(VisibleAnywhere, Category="Components")
 	UStaticMeshComponent* Mesh;
 	UPROPERTY(VisibleAnywhere, Category="Components")
@@ -29,6 +36,11 @@ protected:
 	UBoxComponent* InsideTrigger;
 	UPROPERTY(VisibleAnywhere, Category="Components")
 	UArrowComponent* InsideDirection;
+
+	UPROPERTY(EditAnywhere, Category="Health", meta=(ClampMin=1, UIMin=1))
+	int32 MaxHealth = 200;
+	UPROPERTY(VisibleAnywhere, Category="Health")
+	int32 CurrentHealth = 200;
 	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
