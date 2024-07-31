4a21d0c5de04c239bc6cee88b51f7a0f428b0856
Connor Priest <Kon_Priest@Hotmail.com>
2024-07-31 15:00:09 +0100

    Created save game which is saved when the main menu or restart button is pressed

---
+++ Source/ZombieSiege/Private/Leaderboards/LeaderboardsSaveGame.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Leaderboards/LeaderboardsSaveGame.h"
+
+void ULeaderboardsSaveGame::PrintLeaderboardData(FLeaderboardData Data) const
+{
+	UE_LOG(LogTemp, Warning, TEXT("PlayerName: %s"), *Data.PlayerName);
+	UE_LOG(LogTemp, Warning, TEXT("RoundsSurvived: %d"), Data.RoundsSurvived);
+	UE_LOG(LogTemp, Warning, TEXT("Kills: %d"), Data.Kills);
+	UE_LOG(LogTemp, Warning, TEXT("Score: %d"), Data.Score);
+	UE_LOG(LogTemp, Warning, TEXT("Deaths: %d"), Data.Deaths);
+	UE_LOG(LogTemp, Warning, TEXT("DateAchieved: %s"), *Data.DateAchieved.ToString());
+}
