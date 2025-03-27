91d85d2b0e4b88931f51896776f834ce450c62eb
Connor Priest <Kon_Priest@Hotmail.com>
2025-03-27 13:23:34 +0000

    Updated project and FMOD to 5.5

--- Plugins/FMODStudio/Source/FMODStudio/Private/FMODBlueprintStatics.cpp
+++ Plugins/FMODStudio/Source/FMODStudio/Private/FMODBlueprintStatics.cpp
@@ -1,4 +1,4 @@
-// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2024.
+// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2025.
 
 #include "FMODBlueprintStatics.h"
 #include "FMODAudioComponent.h"
@@ -259,10 +259,10 @@ TArray<FFMODEventInstance> UFMODBlueprintStatics::FindEventInstances(UObject *Wo
             if (Capacity > 0)
             {
                 TArray<FMOD::Studio::EventInstance *> InstancePointers;
-                InstancePointers.SetNum(Capacity, true);
+                InstancePointers.SetNum(Capacity, EAllowShrinking::Yes);
                 int Count = 0;
                 EventDesc->getInstanceList(InstancePointers.GetData(), Capacity, &Count);
-                Instances.SetNum(Count, true);
+                Instances.SetNum(Count, EAllowShrinking::Yes);
                 for (int i = 0; i < Count; ++i)
                 {
                     Instances[i].Instance = InstancePointers[i];
