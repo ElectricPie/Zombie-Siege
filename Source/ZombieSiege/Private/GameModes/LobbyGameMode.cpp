d05e08c3915a8fbd1c3d2d3fbf00b883c050137e
Connor Priest <Kon_Priest@Hotmail.com>
2025-04-24 15:31:47 +0100

    Created Lobby map for multiplayer

---
+++ Source/ZombieSiege/Private/GameModes/LobbyGameMode.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gamemodes/LobbyGameMode.h"
+
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	PlayerCount++;
+}
+
+AActor* ALobbyGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	if (SpawnPoints.Num() == 0)
+	{
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), SpawnPoints);
+
+		SpawnPoints.Sort([](const AActor& A, const AActor& B)
+		{
+			return A.GetName() < B.GetName();
+		});
+	}
+
+	if (PlayerCount < SpawnPoints.Num())
+	{
+		return SpawnPoints[PlayerCount];
+	}
+
+	return Super::ChoosePlayerStart_Implementation(Player);
+}
---
+++ Source/ZombieSiege/Public/Gamemodes/LobbyGameMode.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESIEGE_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+
+private:
+	UPROPERTY()
+	TArray<AActor*> SpawnPoints;
+	int32 PlayerCount = 0;
+};
